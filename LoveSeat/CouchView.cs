915c4ee1ef9440e49065c482e60ac87faa25f8fb
Vincent DARON <vdaron@ask.be>
2011-01-19 22:31:47 +0100

    Add CouchViewDocument and CouchView Classes to manage view creations

---
+++ LoveSeat/CouchView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace LoveSeat
+{
+	public class CouchView
+	{
+		internal CouchView() { }
+
+		public CouchView(string map)
+			:this(map,null)
+		{}
+		public CouchView(string map, string reduce)
+		{
+			Map = map;
+			Reduce = reduce;
+		}
+
+		[JsonProperty("map")]
+		public string Map { get; set; }
+		[JsonProperty("reduce")]
+		public string Reduce { get; set; }
+	}
+}
---
+++ LoveSeat/CouchViewDocument.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace LoveSeat
+{
+	public class CouchViewDocument : BaseDocument
+	{
+		public CouchViewDocument()
+		{
+			Language = "javascript";
+			Views = new Dictionary<string, CouchView>();
+		}
+		public CouchViewDocument(string id)
+			:this()
+		{
+			Id = "_design/" + id;
+		}
+
+		[JsonProperty("language")]
+		public string Language{get;set;}
+
+		[JsonProperty("views")]
+		public Dictionary<string, CouchView> Views { get; internal set; }
+	}
+}
