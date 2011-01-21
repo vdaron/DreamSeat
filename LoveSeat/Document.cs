4c2d3e0a3475296fb38540196302225da471541a
Vincent DARON <vdaron@ask.be>
2011-01-21 18:02:11 +0100

    Put JDocument in its own file

--- LoveSeat/Document.cs
+++ LoveSeat/Document.cs
@@ -7,59 +7,6 @@ using LoveSeat.Interfaces;
 
 namespace LoveSeat
 {
-	public class JDocument : JObject, ICouchDocument
-	{
-		public string Id
-		{
-			get
-			{
-				JToken rev;
-				if (this.TryGetValue("_id", out rev))
-				{
-					return rev.Value<string>();
-				}
-				return null;
-			}
-			set { this["_id"] = value; }
-		}
-		public string Rev
-		{
-			get
-			{
-				JToken rev;
-				if (this.TryGetValue("_rev", out rev))
-				{
-					return rev.Value<string>();
-				}
-				return null;
-			}
-			set { this["_rev"] = value; }
-		}
-		public JDocument()
-		{
-		}
-		public JDocument(JObject jobj)
-			: base(jobj)
-		{
-		}
-		public JDocument(string json)
-			: base(JObject.Parse(json))
-		{
-		}
-
-		public bool HasAttachment
-		{
-			get { return this["_attachments"] != null; }
-		}
-
-		public IEnumerable<string> GetAttachmentNames()
-		{
-			var attachment = this["_attachments"];
-			if (attachment == null) return null;
-			return attachment.Select(x => x.Value<JProperty>().Name);
-		}
-	}
-
 	public class CouchDocument : ICouchDocument
 	{
 		public CouchDocument()
