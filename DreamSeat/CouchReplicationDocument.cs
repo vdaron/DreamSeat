0aa8e72e2811ad9f1eb6cfc68c818b6be0ab5528
Vincent DARON <vdaron@ask.be>
2011-09-20 22:52:10 +0200

    Add UserContext to CouchReplicationDocument

--- DreamSeat/CouchReplicationDocument.cs
+++ DreamSeat/CouchReplicationDocument.cs
@@ -4,22 +4,37 @@ using Newtonsoft.Json;
 
 namespace DreamSeat
 {
+	public class UserContext
+	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
+		[JsonProperty("roles")]
+		public string[] Roles { get; set; }
+	}
+
 	public class CouchReplicationDocument : CouchDocument
 	{
-		private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0,DateTimeKind.Utc);
+		public CouchReplicationDocument()
+		{
+			QueryParams = new Dictionary<string, string>();
+		}
 
 		[JsonProperty(Constants.SOURCE)]
 		public string Source { get; set; }
 		[JsonProperty(Constants.TARGET)]
 		public string Target { get; set; }
 		[JsonProperty(Constants.CONTINUOUS)]
-		public bool? Continuous { get; set; }
+		public bool Continuous { get; set; }
 		[JsonProperty(Constants.QUERY_PARAMS)]
 		public Dictionary<string, string> QueryParams { get; set; }
 		[JsonProperty(Constants.CREATE_TARGET)]
 		public bool? CreateTarget { get; set; }
 		[JsonProperty(Constants.FILTER)]
 		public string Filter { get; set; }
+		[JsonProperty(Constants.USER_CONTEXT)]
+		public UserContext UserContext { get; set; }
+		[JsonProperty(Constants.DOC_IDS)]
+		public string[] DocIds { get; set; }
 
 		[JsonProperty(Constants.REPLICATION_ID)]
 		public string ReplicationId { get; internal set; }
