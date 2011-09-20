ca86602359011ca0ce774c6f05036969aac5ff4f
Vincent DARON <vdaron@ask.be>
2011-09-20 22:52:52 +0200

    Add InstanceStartTime as DateTime in CouchDatabaseInfo

--- DreamSeat/CouchDatabaseInfo.cs
+++ DreamSeat/CouchDatabaseInfo.cs
@@ -1,9 +1,12 @@
 ﻿using Newtonsoft.Json;
+using System;
 
 namespace DreamSeat
 {
 	public class CouchDatabaseInfo
 	{
+		private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
 		[JsonProperty(Constants.COMPACT_RUNNING)]
 		public bool CompactRunning { get; private set; }
 		[JsonProperty(Constants.DB_NAME)]
@@ -17,10 +20,12 @@ namespace DreamSeat
 		[JsonProperty(Constants.DOC_DEL_COUNT)]
 		public int DocDeletedCount { get; private set; }
 		[JsonProperty(Constants.INSTANCE_STARTTIME)]
-		public string InstanceStartTime { get; private set; }
+		public double InstanceStartTimeMs { get; private set; }
 		[JsonProperty(Constants.PURGE_SEQUENCE)]
 		public int PurgeSequence { get; private set; }
 		[JsonProperty(Constants.UPDATE_SEQUENCE)]
-		public int UpdateSequence { get; private set; } 
+		public int UpdateSequence { get; private set; }
+
+		public DateTime InstanceStartTime { get { return Epoch.AddMilliseconds(InstanceStartTimeMs/1000); } }
 	}
 }
